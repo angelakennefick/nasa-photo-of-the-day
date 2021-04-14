295cd9969edce82d3cc07272835300c87bd71b3f
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-04-13 19:39:12 -0500

    Fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 
 function App() {
+  // const API_ID = "";
+  const API_KEY = "qaK0zo9ddCy0RVChjIh0GIJ37ZpF4FNLluLnZxxe";
+
+  const[nasaData, setNasaData] = useState(null);
+
+  useEffect(() => {
+    getNasaData();
+    console.log("Effect getNasaData has run");
+  }, [/*Empty array = api request only runs at page load*/]);
+    
+  const getNasaData = async () => {
+    const nasaData = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+    const data = await nasaData.json(); console.log(data);
+    // setNasaData(data.)
+  }
   return (
     <div className="App">
       <p>
